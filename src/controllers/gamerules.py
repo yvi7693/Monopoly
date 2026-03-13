268fef91ee587dc6054f36247a0f08080d7a9963
Yaroslav Volkov <yvi_7693@icloud.com>
2026-03-13 16:11:26 +0300

    feat: добавлен и реализован класс GameRules

--- src/controllers/gamerules.py
+++ src/controllers/gamerules.py
@@ -1,20 +1,45 @@
+from src.models.businesman import Businessman
+from src.models.dice import Dice
+from src.models.gameboard import Board, Cell
+
+class GameRules:
+
+    board: Board
+    dice: Dice
+    cells: list[Cell]
+
+    def __init__(self, board: Board, cells: list[Cell] = None):
+        if not isinstance(board, Board):  raise TypeError()
+        if not isinstance(cells, list): raise TypeError()
+
+        self.__board = board
+        self.__dice = Dice()
+        self.__cells = cells or []
+
+    def make_move(self, businessman: Businessman):
+        points = self.__dice.throw()
+
+        self.__move_token(points, businessman)
+
+        new_position = businessman.get_position()
+
+        cell = self.board.get_cell(new_position)
+
+        cell.land(businessman)
+
+    def __move_token(self, points: int, businessman: Businessman) -> None:
+        businessman.make_move(points)
+
+        new_position = businessman.get_position()
+
+        if self.board.is_passed_go(new_position):
+            self.__passed_go(businessman)
+
+    def __passed_go(self, businessman: Businessman) -> None:
+        pass
 
 
-Board
-CurrentBusinessman
-Cubes
-Cell
 
-def try_make_move(self):
 
-   points = self.__cubes.throw()
 
-   current_position = self.__current_businessman.get_position()
 
-   new_position = current_position + points
-
-   if new_position > Board.MAX_POSITION:
-       # дать кеш игроку
-       new_position = new_position % Board.MAX_POSITION
-       cell = self.__board.get_cell_by_position(new_position)  # запрос клетки у поля
-       cell.land(self.__current_businessman) # клетка на тебя встали
